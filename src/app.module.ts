6fc982a610c6e510a41bb28da07d28e2346247da
Koblandy <kbaizhigit@outlook.com>
2019-07-30 13:17:42 +0600

    Added websocket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,21 @@
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 
+import { ApiModule } from './api.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { IdeaModule } from './idea/idea.module';
-import { HttpErrorFilter } from './shared/http-error.filter';
-import { LoggingInterceptor } from './shared/logging.intercrptor';
-import { UserModule } from './user/user.module';
-import { CommentModule } from './comment/comment.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [
-    UserModule,
-    IdeaModule,
-    CommentModule,
     TypeOrmModule.forRoot(),
     GraphQLModule.forRoot({
       typePaths: ['./**.*.graphql'],
       context: ({ req }) => ({ headers: req.headers }),
     }),
+    ApiModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useClass: HttpErrorFilter,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor,
-    },
-  ],
+  providers: [AppGateway],
 })
 export class AppModule {}
